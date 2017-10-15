aee273d6a2826a17a2d8c1dbedf524ba4b23a79f
HIDEKUNI Kajita <hide.nba@gmail.com>
2017-10-16 01:22:49 +0900

    open junk file

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,37 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.openJunkfile', () => {
+        const date = new Date();
+        const junkPath = date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate() + '-' +
+                         date.getHours().toString() + date.getMinutes().toString() + date.getSeconds().toString() + '.';
+        const pathLength = junkPath.length                         
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-        // vscode.window.showTextDocument(uri).then((editor)=>{ editor.show()});
-        vscode.window.showInputBox({placeHolder:'input file name'}).then((filename) => {
+        vscode.window.showInputBox({
+            placeHolder:'junkfile path', 
+            value: junkPath, 
+            valueSelection: [pathLength, pathLength]
+        }).then((filename) => {
             if(filename === undefined) { return false; }
 
-            const uri = vscode.Uri.parse('untitled:/tmp/'+filename);
-            vscode.workspace.openTextDocument(uri).then((text)=> { 
-                vscode.window.showTextDocument(text,vscode.ViewColumn.One);
-            })
-        })
+            const baseDir = vscode.workspace.getConfiguration('junkfile').get('dir');
+            const path = process.env.HOME + '/' + baseDir + '/' + filename;
+            openTextDocument('untitled:', path);
+        });
     });
 
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+export function openTextDocument(schrma, path) {
+    const uri = vscode.Uri.parse(schrma+path);
+    vscode.workspace.openTextDocument(uri).then((textDoc)=> { 
+        vscode.window.showTextDocument(textDoc, vscode.ViewColumn.One);
+    }, 
+    (error)=> {
+        openTextDocument('file:', path);
+    });
 }
+
+export function deactivate() {}
